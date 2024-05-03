9b0a948e61f11d0f7b4d0d31b00aa50fdad463a1
AudreyWang716 <166640006+AudreyWang716@users.noreply.github.com>
2024-05-03 01:15:57 -0700

    Add files via upload

---
+++ Homepage.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+# Add page title and contents
+st.title('How Socioeconomic and Infrastructural Factors Influence the Distribution of Music Events Across the U.S.')
+st.markdown('''
+            #### Created by QING WANG
+            ''')
+st.markdown('''
+            <br>
+            Welcome to the Music Events Analysis Across the U.S.!
+            <br>
+            <br> This interactive web application provides a detailed exploration of the distribution and dynamics of music events in various states and cities across the United States. Utilizing  data analytics, this project investigates how factors such as population size, median household income, and infrastructure (the number of airports) impact the number of music events. You can now dive into other pages and the visualizations to uncover insights into the cultural and economic influences shaping the music event landscape in the U.S..
+            <br>
+            <br>
+            ''', unsafe_allow_html=True)
+st.markdown('''
+            _Note: it might take few seconds to upload each page_
+            ''')
